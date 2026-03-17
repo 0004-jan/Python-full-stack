05b83178dd38a5238b5e1031f5eba361aab8102f
Priyanka B <priyankasuthagar@gmail.com>
2026-03-17 16:42:01 +0530

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,9 @@
+a = 17
+b = 5
+print("Addition :", a + b)
+print("Subtraction :", a - b)
+print("Multiplication :", a * b)
+print("Division :", a / b)
+print("Floor division :", a // b)
+print("Modulus :", a % b)
+print("Power :", a ** b)
